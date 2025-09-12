665916582ed957a5f67dd135e411f24c759d4b6e
robert-inkeep <123133375+robert-inkeep@users.noreply.github.com>
2025-09-12 01:12:33 -0400

    Update tracer.ts

--- packages/agents-core/src/utils/tracer.ts
+++ packages/agents-core/src/utils/tracer.ts
@@ -1,7 +1,5 @@
 import { getTracer } from './tracer-factory';
-
-// Import package.json using require() to avoid import assertion syntax
-const pkg = require('../../package.json');
+import pkg from './package.json' with { type: 'json' };
 
 // Pre-configured tracer for agents-core
 export const tracer = getTracer('agents-core', pkg.version);
