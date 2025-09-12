9cf4883bc947112fdbc14e7a7919cfa1dadd7905
shagun-singh-inkeep <shagun.singh@inkeep.com>
2025-09-12 09:31:31 -0400

    no version (#140)

--- agents-run-api/src/utils/tracer.ts
+++ agents-run-api/src/utils/tracer.ts
@@ -1,7 +1,6 @@
 import { getTracer } from '@inkeep/agents-core';
-import pkg from '../../package.json';
 
 // Pre-configured tracer for agents-run-api
-export const tracer = getTracer('agents-run-api', pkg.version);
+export const tracer = getTracer('agents-run-api');
 
 export { setSpanWithError } from '@inkeep/agents-core';
--- packages/agents-core/src/utils/tracer.ts
+++ packages/agents-core/src/utils/tracer.ts
@@ -1,10 +1,7 @@
 import { getTracer } from './tracer-factory';
 
-// Import package.json using require() to avoid import assertion syntax
-const pkg = require('../../package.json');
-
 // Pre-configured tracer for agents-core
-export const tracer = getTracer('agents-core', pkg.version);
+export const tracer = getTracer('agents-core');
 
 // Re-export utilities
 export { setSpanWithError } from './tracer-factory';
